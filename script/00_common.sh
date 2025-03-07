b9a5e8d41f5b25a26bcf4469a49c962823f29f4e
web83info <me@web83.info>
2025-03-07 13:46:36 +0900

    MR42追加
    TODO: radio0, 1, 2が初期化するごとに入れ替わってしまう

--- script/00_common.sh
+++ script/00_common.sh
@@ -343,6 +343,26 @@ if [ "$GLINET_MODEL" = 'WHW03' ]; then
 	wireless_5g2_name=radio0
 fi
 
+if [ "$GLINET_MODEL" = 'MR42' ]; then
+	# 内部構成
+	glinet_has_switch=0
+	glinet_ethernet_lan1_name=eth0
+	glinet_interface_admin=br-vlantap.1
+	glinet_interface_lan=br-vlantap.2
+
+	# SSID最大数
+	wifi_ssid_max=16
+
+	# 無線周波数帯
+	wireless_radio0_name=radio0
+	wireless_radio1_name=radio1
+	wireless_radio2_name=radio2
+	# radio0,1,2が初期化するごとにランダムに入れ替わる
+	# wireless_2g_name=radio0
+	# wireless_5g1_name=radio1
+	# wireless_5g2_name=radio2
+fi
+
 # スペース区切りの文字列を分割し、複数行で処理するためのサブルーチン
 function printf_multi() {
 	array=($2)
